5f3f0f65fe246ba79a2e9335cc06cca4b7a61cdc
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2024-09-20 15:58:29 -0300

    feat: modified main structure
    now the user can choose between seeing the inventory or adding into it

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+from Inventario import Inventario
 from Mercadoria import Mercadoria
 
 def main() -> int:
+	inventario: Inventario = Inventario()
 
 	while(True):
-		nome = input("Nome do produto: ")
-		tipo = input("Tipo do produto: ")
-		peso = float(input("Peso do produto (gramas): "))
-		custo = float(input("Qual é o preço do produto? "))
+		op = input("Gostaria de [v]er itens que estão no inventário u [i]nseri-los? ")
 
-		mercadoria: Mercadoria = Mercadoria(nome, tipo, peso, custo)
+		if(op == 'i'):
+			nome = input("Nome do produto: ")
+			tipo = input("Tipo do produto: ")
+			peso = float(input("Peso do produto (gramas): "))
+			custo = float(input("Qual é o preço do produto? "))
 
+			mercadoria: Mercadoria = Mercadoria(nome, tipo, peso, custo)
+			inventario.adicionarMercadoria(mercadoria)
+
+		if(op == 'v'):
+			inventario.mostrarInventario()
 
 	return 0
 
