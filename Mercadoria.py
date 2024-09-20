7d0dd1e703c3296eba8cb931a8c8ad47836a1138
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2024-09-20 15:45:59 -0300

    feat: Added new method gerarInformacao

--- Mercadoria.py
+++ Mercadoria.py
@@ -1,5 +1,14 @@
 class Mercadoria:
 	def __init__(self, nome: str, tipo: str, peso: float, preco: float) -> None:
+		self.nome = nome
 		self.tipo = tipo
 		self.peso = peso
 		self.preco = preco
+
+	def gerarInformacao(self) -> dict:
+		return {
+			"nome" : self.nome,
+			"tipo" : self.tipo,
+			"peso" : self.peso,
+			"preco" : self.preco
+		}
