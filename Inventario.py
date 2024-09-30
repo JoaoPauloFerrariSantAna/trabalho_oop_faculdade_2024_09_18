330da80e7b21bcb0acb9a94efb3596b2583f969b
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2024-09-30 14:40:09 -0300

    feat: now Inventario will show more detailed info

--- Inventario.py
+++ Inventario.py
@@ -3,9 +3,18 @@ class Inventario:
 		# aparentemente esse "_" mostra que não é para
 		# usar "_inventario" fora da classe
 		self._inventario = []
-	
+
 	def adicionarMercadoria(self, item: dict[str, str | float]) -> None:
 		self._inventario.append(item)
-	
+
 	def mostrarInventario(self) -> None:
-		print(self.inventario)
+		print("Items no inventário:")
+
+		# vai loopar por todo o inventário
+		for item in range(len(self._inventario)):
+			nome_merc = self._inventario[item].nome
+			tipo_merc = self._inventario[item].tipo
+			peso_merc = self._inventario[item].peso
+			preco_merc = self._inventario[item].preco
+
+			print(f"- {nome_merc} ({tipo_merc}): R${preco_merc}, {peso_merc} gramas")
